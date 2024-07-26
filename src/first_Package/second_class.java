82731b7a3ff3dcf756cebe4dead21119c6cec2af
mb-gitt <madhulika.bhalerao79.mb@gmail.com>
2024-07-26 12:13:22 +0530

    modification & add in new branch

---
+++ src/first_Package/second_class.java
@@ -0,0 +1,13 @@
+package first_Package;
+
+public class second_class {
+
+	public static void main(String[] args) {
+		test();
+
+	}
+	public static void test() {
+		System.out.println(40+50);
+	}
+
+}
