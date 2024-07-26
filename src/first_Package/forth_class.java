fd1b3bc0382020336a52ccd4a4af017d51074470
mb-gitt <madhulika.bhalerao79.mb@gmail.com>
2024-07-26 12:22:03 +0530

    new classes

---
+++ src/first_Package/forth_class.java
@@ -0,0 +1,9 @@
+package first_Package;
+
+public class forth_class {
+
+	public static void main(String[] args) {
+		System.out.println(" This is a fourth class ");
+	}
+
+}
---
+++ src/first_Package/third_class.java
@@ -0,0 +1,13 @@
+package first_Package;
+
+public class third_class {
+
+	public static void main(String[] args) {
+	int a=10; 
+	int b=20;
+	int c=30;
+	if((a<b)&(a<c)) {
+	System.out.println("a is smallest int");
+	}}
+
+}
